53fbf3ab5996a2e44cc159233f3d525502674eb3
katsihuin <katsihuin@gmail.com>
2017-01-15 16:38:52 -0500

    function to validate last name added

--- js/main.js
+++ js/main.js
@@ -10,4 +10,44 @@ function producePrompt(message, promptLocation, color)
 	document.getElementById(promptLocation).style.color = color;
 }
 
+/* Valida Nombre*/
+function validateName()
+{
+	var name =  document.getElementById("commentName").value;
+	if (name.length == 0)
+	{
+		producePrompt("Tu Nombre es requerido", "commentNamePrompt", "red");
+		return false;
+	}
+	else if (!name.match(/^[A-Z][a-z]*[a-zA-Z]$/)) 
+	{
+		producePrompt("Compruebe que la primera letra de su Nombre sea mayúscula y contenga SOLO caracteres de la A-Z", "commentNamePrompt", "red");
+		return false;
+	}
+	else 
+	{
+		producePrompt("Bienvenido(a) " + name, "commentNamePrompt", "green");
+		return true;
+	}	
+}
 
+/* Valida Apellido*/
+function validateLastName()
+{
+	var lastName = document.getElementById("commentLastName").value;
+	if (lastName.length == 0)
+	{
+		producePrompt("Tu Apellido es requerido", "commentLastNamePrompt", "red");
+		return false;
+	}
+	else if (!lastName.match(/^[A-Z][a-z]*[a-zA-Z]$/)) 
+	{
+		producePrompt("Compruebe que la primera letra de su Apellido sea mayúscula y contenga SOLO caracteres de la A-Z", "commentLastNamePrompt", "red");
+		return false;
+	}
+	else 
+	{
+		producePrompt("Apellido Válido ✔", "commentLastNamePrompt", "green");
+		return true;
+	}	
+}
