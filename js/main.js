4413e972ced466ea643789bbbd05195f314ba691
katsihuin <katsihuin@gmail.com>
2017-01-15 16:35:48 -0500

    adding function to validate name

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ function validateForm()
 	/* Escribe tú código aquí */	
 }
 
-
+/* Envia Mensaje al usuario*/
+function producePrompt(message, promptLocation, color)
+{
+	document.getElementById(promptLocation).innerHTML = message;
+	document.getElementById(promptLocation).style.color = color;
+}
 
 
