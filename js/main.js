5bea46f369dac0e65c963da3793416132b024a93
Katherin Sihuin <katsihuin@gmail.com>
2017-01-16 12:12:55 -0500

    fixing capital letters in name and last name input

--- js/main.js
+++ js/main.js
@@ -42,7 +42,7 @@ function validateName()
 		producePrompt("Tu Nombre es requerido", "commentNamePrompt", "red");
 		return false;
 	}
-	else if (!name.match(/^[A-Z][a-z]+[a-zA-Z]+[\s]+[A-Z][a-z]+[a-z]$/)) 
+	else if (!name.match(/^[a-zA-Z\s]*$/)) 
 	{
 		producePrompt("Compruebe que la primera letra de su Nombre(s) sea mayúscula y contenga SOLO caracteres de la A-Z", "commentNamePrompt", "red");
 		return false;
@@ -63,7 +63,7 @@ function validateLastName()
 		producePrompt("Tu Apellido es requerido", "commentLastNamePrompt", "red");
 		return false;
 	}
-	else if (!lastName.match(/^[A-Z][a-z]+[a-zA-Z]+[\s]+[A-Z][a-z]+[a-z]$/)) 
+	else if (!lastName.match(/^[a-zA-Z\s]*$/)) 
 	{
 		producePrompt("Compruebe que la primera letra de su Apellido(s) sea mayúscula y contenga SOLO caracteres de la A-Z", "commentLastNamePrompt", "red");
 		return false;
@@ -165,7 +165,7 @@ function validateInfo()
 	var info = document.getElementById("commentInfo").value;
 	if (info.checked==true) 
 	{
-		producePrompt("¡Gracias! Estaremos en contacto muy pronto.", "commentInfoPrompt", "blue");
+		producePrompt("¡Gracias! Estaremos en contacto contigo muy pronto.", "commentInfoPrompt", "blue");
 		return true;
 	}
 }
